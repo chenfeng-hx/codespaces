4786bcf25f3acbd67536dc77306e164234395ffb
chenfeng-hx <text021578@outlook.com>
2023-04-12 13:25:51 +0800

    no important

---
+++ 数据结构js实现/线性表的顺序存储.js
@@ -0,0 +1,24 @@
+// 使用数组当做顺序存储
+const arr = [];
+
+/**
+ * 实现功能：
+ * 1. 增加(插入)
+ * 2. 删除
+ * 3. 更改
+ * 4. 查找
+ */
+
+
+function add(arr, value, index) {
+    // 默认在不输入坐标的情况下插入到表尾
+    if (index === undefined) {}
+        // 先判断要插入的位置是否合理
+        if (index < 0) console.log('位置不合法！');
+        // 插入
+        else if (index <= arr.length - 1) {
+            // 说明在现有数组内部，需要
+            
+        }
+    return arr;
+}
